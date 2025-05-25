113f4b968af820a57840c16f5e8ed85096f43998
Ivan Ivon <ivan.ivon@gmail.com>
2025-05-25 17:03:30 +0300

    Add jump list timer tasks

---
+++ Hourglass/Extensions/DisposableExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hourglass.Extensions;
+
+internal static class DisposableExtensions
+{
+    public static IDisposable CreateDisposable(this Action action) =>
+        new DisposableAction(action);
+
+    private class DisposableAction : IDisposable
+    {
+        private readonly Action _action;
+
+        private bool _disposed;
+
+        public DisposableAction(Action action) =>
+            _action = action;
+
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _disposed = true;
+                _action();
+            }
+        }
+    }
+}
