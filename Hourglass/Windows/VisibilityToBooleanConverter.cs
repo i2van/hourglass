42b83b5ee59b4101a43b7bbb7bf7c5d2cc58d77b
Ivan Ivon <ivan.ivon@gmail.com>
2025-06-16 17:14:42 +0300

    Show all thumbnail buttons. (#53)

---
+++ Hourglass/Windows/VisibilityToBooleanConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Hourglass.Windows;
+
+public sealed class VisibilityToBooleanConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is Visibility.Visible;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        throw new NotSupportedException();
+}
